6de4f47c7fe544c5660f055d667a500f16953376
Kiran Kharade <kiran.kharade@reanfoundation.org>
2021-06-23 18:27:51 +0530

    Fixed issues while running the tests. Some cleanup on storage service. Added GcpHelper.

--- fhir/src/tests/patient.test.ts
+++ fhir/src/tests/patient.test.ts
@@ -3,14 +3,32 @@ import { PatientMapper } from "../types/mappers/patient.mapper";
 
 
 describe('Patient resource: Storage, retrieval', () => {
-    test('Given patient domain model, store patient resource to fhir interface, then returned id is valid uuid', async (done) => {
+    it('Given patient domain model, store patient resource to fhir interface, then returned patient details are valid.', async () => {
 
         var model = PatientMapper.convertJsonObjectToDomainModel();
         var patientFhirId = await Loader.PatientStore.create(model);
         var patientFhirResource = await Loader.PatientStore.getById(patientFhirId);
+
+        //Assertions
         var extractedName = patientFhirResource.name[0].family;
         expect(extractedName).toEqual(model.LastName);
-        
-        done();
+
+        var extractedBirthdate = patientFhirResource.birthDate;
+        expect(extractedBirthdate).toEqual(model.BirthDate);
+
+        var extractedGender = patientFhirResource.gender;
+        expect(extractedGender).toEqual(model.Gender.toLowerCase());
+
+        var phoneElement = patientFhirResource.telecom.find(function (e) {
+            return e.system === 'phone';
+        });
+        var extractedPhone = phoneElement ? phoneElement.value : '';
+        expect(extractedPhone).toEqual(model.Phone);
+
+        var emailElement = patientFhirResource.telecom.find(function (e) {
+            return e.system === 'email';
+        });
+        var extractedEmail = emailElement ? emailElement.value : '';
+        expect(extractedEmail.toLowerCase()).toEqual(model.Email.toLowerCase());
     });
 });
